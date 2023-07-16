66cb06a9b1a84caf5bc86e16ca3bb761bde12246
zNotChill <znotchill@znci.dev>
2023-07-16 03:54:30 +0100

    Fix copy and pasting

---
+++ app/src/commands/theme.js
@@ -0,0 +1,10 @@
+const { Command } = require("../commands");
+
+module.exports = {
+  name: "theme",
+  description: "Changes the current theme",
+  execute: (args, terminal, call) => {
+    
+    call(0);
+  },
+};
